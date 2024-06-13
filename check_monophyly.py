57b54b98a2761377b1acae5280245f729b3c3aff
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2024-06-13 23:49:18 +0300

    Add comments to check_monophyly.py

--- check_monophyly.py
+++ check_monophyly.py
@@ -5,7 +5,7 @@ Created on Sun Nov  5 23:35:18 2023
 
 @author: gospozha
 """
-
+# Checking monophyly of the clades with ete3
 
 from ete3 import Tree
 import os
@@ -16,6 +16,7 @@ rad1=[]
 rad2=[]
 rad3=[]
 
+# Path to the directory; iterating through multiple tree files
 os.chdir("/home/gospozha/EvoGen/monoph/")
 for filename in os.listdir('/home/gospozha/EvoGen/monoph/aln2/root/'):
     if filename.endswith('.treefile'):
@@ -23,6 +24,7 @@ for filename in os.listdir('/home/gospozha/EvoGen/monoph/aln2/root/'):
   #  line = '/home/gospozha/EvoGen/monoph/aln/root/OrthoGroup9.fasta.contree'
 
             tax.append(filename)
+        # Listing the species which we want to check
             rad1.append(t.check_monophyly(values=[ "sibiricus", "schisticolor" ], target_attr="name")[1])
             rad2.append(t.check_monophyly(values=["centralis", "glareolus", "semicanus", 
                                                   "tuvinicus", "rutilus", "rufocanus",
@@ -32,6 +34,7 @@ for filename in os.listdir('/home/gospozha/EvoGen/monoph/aln2/root/'):
                                                   "agrestis", "gregalis", "pennsylvanicus",
                                                    "brandtii","mandarinus",
                                                   "ochrogaster"], target_attr="name")[1])
+# Creating a dataframe with results and saving it
 df = pd.DataFrame(list(zip(tax, rad1, rad2, rad3)),
                columns =['Name', 'rad1', 'rad2', 'rad3'])
 df.to_csv("/home/gospozha/EvoGen/monoph/aln2/ete3-4.csv")
